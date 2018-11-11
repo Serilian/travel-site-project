a8df65c2be2c6eb32577cb1a0e8fba6a93e47fa5
Filip <fhagno@yahoo.com>
2018-11-11 11:35:42 +0100

    adding webpack, js modularity and icon animation

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -19,9 +19,18 @@ gulp.task('watch', () => {
     watch('./app/assets/styles/**/*.css', function () {
         gulp.start('cssInject');
     });
+
+    watch('./app/assets/scripts/**/*.js', ()=> {
+        gulp.start('scripts-refresh');
+    })
+
 });
 
 gulp.task('cssInject', ['styles'] ,()=> {
     return gulp.src('./app/temp/styles/styles.css')
         .pipe(browserSync.stream());
+});
+
+gulp.task('scripts-refresh',['webpack'], ()=> {
+    browserSync.reload();
 });
---
+++ gulp/tasks/webpack.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp');
+const webpack = require('webpack');
+
+
+gulp.task('webpack', function (callback) {
+ webpack(require('../../webpack.config.js'), (err, stats)=>{
+
+     if(err) {
+         console.log(err.toString());
+     }
+
+     console.log(stats.toString());
+
+     callback();
+ });
+});
